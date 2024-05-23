3506e1ffc94769885fc4897a67371c5708224667
Diana Lee <diana.lee@devcra.com>
2024-05-23 15:30:27 +0900

    add: delete&publish

---
+++ pages/api/post/index.ts
@@ -0,0 +1,17 @@
+import { getSession } from "next-auth/react";
+import { AuthOptions } from "../auth/[...nextauth]";
+import prisma from "../../../lib/prisma";
+import { getServerSession } from "next-auth";
+
+export default async function handle(req, res) {
+  const { title, content } = req.body;
+  const session = await getServerSession(req, res, AuthOptions);
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      content: content,
+      author: { connect: { email: session?.user?.email } },
+    },
+  });
+  res.json(result);
+}
