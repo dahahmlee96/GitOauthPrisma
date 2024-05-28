1f35fe81ca926caf2d580b65c44e5cf6147b084b
Diana Lee <diana.lee@devcra.com>
2024-05-28 16:05:31 +0900

    add password

--- prisma/migrations/20240528070507_init/migration.sql
+++ prisma/migrations/20240528070507_init/migration.sql
@@ -5,6 +5,8 @@ CREATE TABLE "Post" (
     "content" TEXT,
     "published" BOOLEAN NOT NULL DEFAULT false,
     "authorId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
@@ -42,6 +44,7 @@ CREATE TABLE "Session" (
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "name" TEXT,
     "email" TEXT,
     "emailVerified" TIMESTAMP(3),
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,33 +1,40 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
 const prisma = new PrismaClient();
 
 async function main() {
+  const hashedPassword1 = await bcrypt.hash("1234", 10);
+  const hashedPassword2 = await bcrypt.hash("1234", 10);
+
   const sunimkoh = await prisma.user.upsert({
-    where: { email: 'sunimkohlee@naver.com' },
+    where: { email: "sunimkohlee@naver.com" },
     update: {},
     create: {
-      email: 'sunimkohlee@naver.com',
-      name: 'sunimkohlee',
+      email: "sunimkohlee@naver.com",
+      password: hashedPassword1,
+      name: "sunimkohlee",
       posts: {
         create: {
-          title: 'sunimkoh',
-          content: 'I am sunimkohlee',
+          title: "sunimkoh",
+          content: "I am sunimkohlee",
           published: true,
         },
       },
     },
   });
-  
+
   const dahahmlee = await prisma.user.upsert({
-    where: { email: 'dahahmlee@gmail.com' },
+    where: { email: "dahahmlee@gmail.com" },
     update: {},
     create: {
-      email: 'dahahmlee@gmail.com',
-      name: 'dahahmlee',
+      email: "dahahmlee@gmail.com",
+      password: hashedPassword2,
+      name: "dahahmlee",
       posts: {
         create: {
-          title: 'dahahmlee',
-          content: 'I am dahahmlee',
+          title: "dahahmlee",
+          content: "I am dahahmlee",
           published: true,
         },
       },
@@ -36,7 +43,7 @@ async function main() {
 }
 
 main()
-  .catch(e => {
+  .catch((e) => {
     console.error(e);
     process.exit(1);
   })
